69f324a598477e45a255a236a3af0366a7f34ee8
Андрей Увиков <uvikmedium@mail.ru>
2024-01-18 18:21:54 +0300

    add headers.php with CORS headers

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
+include 'headers.php';
 
 echo 'hello';
--- tickets.php
+++ tickets.php
@@ -1,7 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
+include 'headers.php';
 
 include 'DbConnect.php';
 
